63d397588ec1f94b59d00623f175ad54fe9b7fa3
jadikhalid <jadikhalid@gmail.com>
2026-07-11 18:10:46 +0100

    Initial commit: mise en place du frontend et ignore

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl ">Hello</h1>
+      <Button variant="outline">This is a Button</Button>
+    </>
+  );
+}
+
+export default App;
